e54ba2d0d47d51692c06c118a77ec0f3a8bd34ae
ryan neo jia hao <ryandacow@ryans-MacBook-Air.local>
2024-12-08 15:07:13 +0800

    Debug auto-delete 1

--- telebot/engine/add_expense.py
+++ telebot/engine/add_expense.py
@@ -7,19 +7,16 @@ import asyncio, threading
 PURPOSE, PAYER, AMOUNT, BENEFICIARIES, SPLIT = range(5)
 
 async def add_expense(update: Update, context: CallbackContext):
-    context.user_data["bot_message"] = await update.message.reply_text("What was the expense for?")
+    bot_message = await update.message.reply_text("What was the expense for?")
+    await context.bot.deleteMessage(chat_id=bot_message.chat_id, message_id=bot_message.message_id)
     return PURPOSE
 
 async def add_purpose(update: Update, context: CallbackContext):
     context.user_data["purpose"] = update.message.text
-    bot_message = context.user_data["bot_message"]
-    reply_message = context.user_data["purpose"]
-    await asyncio.sleep(5)  # Optional: delay to allow user to read
+    await context.bot.deleteMessage(chat_id=update.message.chat_id, message_id=update.message.message_id)
+    
+    bot_message = await update.message.reply_text("Who paid?")
     await context.bot.deleteMessage(chat_id=bot_message.chat_id, message_id=bot_message.message_id)
-    await context.bot.deleteMessage(chat_id=reply_message.chat_id, message_id=reply_message.message_id)
-    
-    await update.message.reply_text("Who paid?")
-    
     return PAYER
 
 async def add_payer(update: Update, context: CallbackContext):
